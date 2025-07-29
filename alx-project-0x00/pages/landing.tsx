35cbf5f1c4892a073c7da1e8f466550d926a3add
amarisodiq <amarisodiq@gmail.com>
2025-07-29 14:17:39 +0100

    create button components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,12 +1,15 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 
 const Landing: React.FC = () => {
     return (
         <div>
             <h1 className="text-xl font-extralight">Landing Page</h1>
             <Card />
-            <Card />
-            <Card />
+
+            <Button title="Rounded Sm" styles="px-3 py-1 mx-2 bg-blue-500 text-sm rounded-sm"/>
+            <Button title="Rounded md" styles="px-3 py-1 mx-2 bg-green-500 text-md rounded-md"/>
+            <Button title="Rounded full" styles="px-3 py-1 mx-2 bg-green-500 text-lg rounded-full"/>
         </div>
     )
 }
