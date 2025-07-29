3f01cd406da49828aad1e7b7f7988d57c9138207
amarisodiq <amarisodiq@gmail.com>
2025-07-29 14:19:39 +0100

    add a button

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -9,6 +9,7 @@ const Landing: React.FC = () => {
 
             <Button title="Rounded Sm" styles="px-3 py-1 mx-2 bg-blue-500 text-sm rounded-sm"/>
             <Button title="Rounded md" styles="px-3 py-1 mx-2 bg-green-500 text-md rounded-md"/>
+            <Button title="Rounded lg" styles="px-3 py-1 mx-2 bg-green-500 text-md rounded-lg"/>
             <Button title="Rounded full" styles="px-3 py-1 mx-2 bg-green-500 text-lg rounded-full"/>
         </div>
     )
